2dddfcc8904c3439fe7019bb668085759cfc600a
millerr2 <millerr2@cmc306-12.mathcs.carleton.edu>
2021-06-28 09:22:26 -0500

    with chrontab comment

--- fetch_updates.py
+++ fetch_updates.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+#make sure to go in and edit your chrontab by typing vi chrontab -e then once you get in there changing it to visual mode before adding the line 08*** python3 /path/to/test_script.py filling in the path there
+
 base_url = 'https://cs.carleton.edu/faculty/mtie/lab-updates-2021/'
 headers = requests.utils.default_headers()
 headers.update({'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",})
@@ -21,4 +23,3 @@ def get_files(file_num):
 
 
 get_files(get_file_num())
-
