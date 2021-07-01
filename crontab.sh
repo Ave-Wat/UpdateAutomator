5d84a22f634f734a95614d34dc5684e4cb22ceff
Ave-Wat <wattsa2@carleton.edu>
2021-07-01 14:10:59 -0500

    attempting to fix permissions

--- crontab.sh
+++ crontab.sh
@@ -1,1 +1,1 @@
-(crontab -l 2>/dev/null; echo "* * * * * /usr/bin/python3 /Users/cslab/Desktop/UpdateAutomator/fetch_updates.py") | crontab -
+(crontab -l 2>/dev/null; echo "* * * * * /usr/bin/python3 /Users/wattsa2/Desktop/UpdateAutomator/fetch_updates.py") | crontab -
